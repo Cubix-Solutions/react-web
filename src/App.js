0ccc5956132404ef8db694d08522b4abdc6ebcae
A-Samod <asamod999@gmail.com>
2023-03-17 23:19:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is a sample core for our web page .
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Cubix-Solutions"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Join with us
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
